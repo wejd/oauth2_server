7c63ea505309440c96a48db69f96c62eea6306cf
avempaceVoice <voice-dev@avempace-wireless.com>
2017-06-29 11:38:00 +0100

    add intent whic user is connected

--- server.js
+++ server.js
@@ -341,6 +341,7 @@ router.get('/playtrack', authController.isAuthenticated, function(req, res, next
 router.get('/playnext', authController.isAuthenticated, function(req, res, next) {
     i = 0;
     j = 0
+    console.log(req.user)
     speakerController.findSpeakerByOwner(req.user.id, function(listSpeaker) {
         listSpeaker.forEach(function(speaker) {
 
